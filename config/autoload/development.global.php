58b2959f1505ec2eb771c3fdad244c4af94aab19
Judzhin Miles <info@msbios.com>
2017-11-26 23:36:52 +0200

    Lazy Composer Commiter

--- config/autoload/development.global.php
+++ config/autoload/development.global.php
@@ -3,7 +3,10 @@
  * @access protected
  * @author Judzhin Miles <info[woof-woof]msbios.com>
  */
+namespace MSBios\Portal\Doctrine;
+
 return [
+
     'doctrine' => [
         'connection' => [
             'orm_default' => [
@@ -14,6 +17,12 @@ return [
                     'dbname' => 'portal.dev',
                 ],
             ],
+        ],
+    ],
+
+    \MSBios\Assetic\Module::class => [
+        'paths' => [
+            __DIR__ . '/../../vendor/msbios/theme/themes/default/public',
         ],
     ],
 ];
